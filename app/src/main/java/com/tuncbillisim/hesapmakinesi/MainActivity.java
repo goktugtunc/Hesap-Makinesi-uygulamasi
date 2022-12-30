48522aede9afa47e8bb57896bad7dae5d960491a
Goktug <gktgtunc@gmail.com>
2022-12-31 02:30:47 +0300

    9 yeni işlem eklendi ve görünüm iyileştirildi!

--- app/src/main/java/com/tuncbillisim/hesapmakinesi/MainActivity.java
+++ app/src/main/java/com/tuncbillisim/hesapmakinesi/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             double number1 = Double.parseDouble(sayi1.getText().toString());
             double number2 = Double.parseDouble(sayi2.getText().toString());
             double sonucum = number1 + number2;
-            sonuc.setText("" + sonucum);
+            sonuc.setText(number1 + " + " + number2 + " = " + sonucum);
         }
     }
     public void cikarma(View view)
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             double number1 = Double.parseDouble(sayi1.getText().toString());
             double number2 = Double.parseDouble(sayi2.getText().toString());
             double sonucum = number1 - number2;
-            sonuc.setText("" + sonucum);
+            sonuc.setText(number1 + " - " + number2 + " = " + sonucum);
         }
     }
     public void carpma(View view)
@@ -60,7 +60,128 @@ public class MainActivity extends AppCompatActivity {
             double number1 = Double.parseDouble(sayi1.getText().toString());
             double number2 = Double.parseDouble(sayi2.getText().toString());
             double sonucum = number1 * number2;
-            sonuc.setText("" + sonucum);
+            sonuc.setText(number1 + " * " + number2 + " = " + sonucum);
+        }
+    }
+    public void mod(View view)
+    {
+        if (sayi1.getText().toString().matches("") || sayi2.getText().toString().matches("") || sayi1.getText().toString().matches("") && sayi2.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else
+        {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double number2 = Double.parseDouble(sayi2.getText().toString());
+            double sonucum = number1 % number2;
+            sonuc.setText(number1 + " % " + number2 + " = " + sonucum);
+        }
+    }
+    public void log(View View)
+    {
+        if (sayi1.getText().toString().matches("") || sayi2.getText().toString().matches("") || sayi1.getText().toString().matches("") && sayi2.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else
+        {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double number2 = Double.parseDouble(sayi2.getText().toString());
+            double sonucum = Math.log(number2) / Math.log(number1);
+            sonuc.setText(number1 + " logaritma " + number2 + " = " + sonucum);
+        }
+    }
+    public void us(View view)
+    {
+        if (sayi1.getText().toString().matches("") || sayi2.getText().toString().matches("") || sayi1.getText().toString().matches("") && sayi2.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else
+        {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double number2 = Double.parseDouble(sayi2.getText().toString());
+            double sonucum = Math.pow(number1, number2);
+            sonuc.setText(number1 + " üssü " + number2 + " = " + sonucum);
+        }
+    }
+    public void sin(View view)
+    {
+        if (sayi1.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else
+        {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double sonucum = Math.sin(number1);
+            sonuc.setText("sin(" + number1 + ") = " + sonucum);
+        }
+    }
+    public void cos(View view)
+    {
+        if (sayi1.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else
+        {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double sonucum = Math.cos(number1);
+            sonuc.setText("cos(" + number1 + ") = " + sonucum);
+        }
+    }
+    public void tan(View view)
+    {
+        if (sayi1.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double sonucum = Math.tan(number1);
+            sonuc.setText("tan(" + number1 + ") = " + sonucum);
+        }
+    }
+    public void cot(View view)
+    {
+        if (sayi1.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else
+        {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double asdasd = Math.cos(number1);
+            double deneme = Math.sin(number1);
+            double sonucum = asdasd / deneme;
+            sonuc.setText("cot(" + number1 + ") = " + sonucum);
+        }
+    }
+    public void ln(View view)
+    {
+        if (sayi1.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else
+        {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double sonucum = Math.log10(number1);
+            sonuc.setText("ln(" + number1 + ") = " + sonucum);
+        }
+    }
+    public void karekok(View view)
+    {
+        if (sayi1.getText().toString().matches(""))
+        {
+            sonuc.setText("Lütfen Sayı Giriniz!");
+        }
+        else
+        {
+            double number1 = Double.parseDouble(sayi1.getText().toString());
+            double sonucum = Math.sqrt(number1);
+            sonuc.setText("Karekök " + number1 + " = " + sonucum);
         }
     }
     public void bolme(View view)
@@ -78,7 +199,7 @@ public class MainActivity extends AppCompatActivity {
             double number1 = Double.parseDouble(sayi1.getText().toString());
             double number2 = Double.parseDouble(sayi2.getText().toString());
             double sonucum = number1 / number2;
-            sonuc.setText("" + sonucum);
+            sonuc.setText(number1 + " / " + number2 + " = " + sonucum);
         }
     }
 }
